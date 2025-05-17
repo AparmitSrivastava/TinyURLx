e2d89fe3c0d63b67df2970636e1db7a00fa5c9b1
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-05-17 22:14:33 +0530

    dynamicRouting

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -2,7 +2,6 @@
 import { redirect } from "next/navigation"
 import clientPromise from "@/lib/mongodb"
 
-
 export default async function Page({ params }) {
     const shorturl = (await params).shorturl
 
